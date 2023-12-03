ed7a28a446dd1e62f879dd3121d5c53b78903dd3
SteakWarrior <87113326+SteakWarrior@users.noreply.github.com>
2023-12-03 10:56:30 +0800

    Update my_resnet.py

--- cifar-10_100/my_resnet.py
+++ cifar-10_100/my_resnet.py
@@ -68,7 +68,7 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 8, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(8)
-        self.cl = nn.Conv2d(8, 8, kernel_size=1, bias=False)       #compatibility layer
+        self.cl = nn.Conv2d(8, 8, kernel_size=1, bias=False)       # ReDe layer
         self.layer1 = self._make_layer(block, 32, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 64, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 128, num_blocks[2], stride=2)
@@ -85,8 +85,8 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         # out = F.relu(self.bn1(self.conv1(x)))
-        # out = self.cl(out)
-        # self.semi_f = out
+        out = self.cl(out)
+        self.semi_f = out
         out = self.layer1(x)
         out = self.layer2(out)
         out = self.layer3(out)
